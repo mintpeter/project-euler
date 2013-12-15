ffe51398ae1217e830d7b5ec305d833a7ff4b6ea
mintpeter <zpmarvel@gmail.com>
2013-12-15 02:47:08 -0500

    Python - Problem 1
    I solved Problem 1 in Python.

--- 1.py
+++ 1.py
@@ -2,4 +2,23 @@
 ##
 ## Find the sum of all the multiples of 3 or 5 below 1000.
 
-define 
+def multiple3or5(n):
+	if ((n % 3) == 0) or ((n % 5) == 0):
+		return True
+	else:
+		return False
+
+def main():
+	i = 1
+	acc = 0
+	
+	while i < 1000:
+		if multiple3or5(i):
+			acc += i
+			i += 1
+		else:
+			i += 1
+	
+	return acc
+
+print main()
