6d46ae45f11be24eec05523ff4857c2776f728f6
Zack Marvel <zpmarvel@gmail.com>
2013-12-16 16:45:18 -0500

    Python - Problem 10 Changes
    I decided to follow the same algorithm I did with Scheme. I was unable to successfully implement a sieve algorithm.

--- 10.py
+++ 10.py
@@ -3,25 +3,45 @@
 ## Find the sum of all the primes below two million.
 
 import time
-from math import sqrt
+from math import sqrt, ceil
+
+#def main():
+#    rng = range(2, 2 * (10 ** 6))
+#    primes = range(2, 2 * (10 ** 6))
+#    for n in rng:
+#        rt = sqrt(n)
+#        for m in primes:
+#            if m <= rt:
+#                if n % m == 0:
+#                    if n in primes:
+#                        primes.remove(n)
+#            else: break
+#    acc = 0
+#    for n in rng:
+#        acc += n
+#    
+#    return rng[:10]
 
 def main():
-    rng = range(2, 2000000)
+    rng = range(3, 2 * (10 ** 6))
+    primes = [2]
+    
     for n in rng:
         rt = sqrt(n)
-        for m in rng:
+        for m in primes:
             if m <= rt:
                 if n % m == 0:
-                    rng.remove(n)
                     break
             else:
-                break
+                primes.append(m)
     
     acc = 0
-    for n in rng:
+    
+    for n in primes:
         acc += n
     
     return acc
+
 
 start_time = time.time()
 
