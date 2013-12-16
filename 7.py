294e1e22f528833cae7ab7510a385cb7cefbc82d
mintpeter <zpmarvel@gmail.com>
2013-12-16 10:31:37 -0500

    Python Problem 7
    Problem returns correct answer in 0.171999931335s.

--- 7.py
+++ 7.py
@@ -3,24 +3,27 @@
 ## What is the 10 001st prime number?
 
 from math import sqrt
+import time
 
 def main():
     primes = [2]
     i = 3
     j = 1
     
-    while j != 10001:
+    while j < 10001:
         rt = sqrt(i)
         for prime in primes:
-            if prime < rt:
+            if prime <= rt:
                 if i % prime == 0:
                     i += 2
                     break
             else:
                 primes.append(i)
+                i += 2
                 j += 1
                 break
     
-    return primes[0]
+    return primes[j - 1]
 
-print main()
+start_time = time.time()
+print main(), time.time() - start_time
